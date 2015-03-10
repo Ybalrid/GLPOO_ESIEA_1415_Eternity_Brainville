c0f8f8ffb22c301a6fa8f13e99262ab2a58cc7d7
Ybalrid <ybalrid@cecmu.org>
2015-03-10 11:55:48 +0100

    Grand nétoyage

--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,16 +1,10 @@
 package main.java;
 
-import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.GridBagLayout;
-import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -18,7 +12,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 
-public class MainWindow extends JPanel {
+public class MainWindow {
 	
 	private JFrame frame;
 	private int w = 1024;
@@ -72,17 +66,5 @@ public class MainWindow extends JPanel {
 	
 		frame.add(p);
 		p.setVisible(true);
-		}
-	
-	@Override
-	public void paint(Graphics g)
-	{
-		super.paint(g);
-		Graphics2D g2 = (Graphics2D)g;
-		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		
-		g2.setColor(Color.BLUE);
-		g2.fillRect(0, 0, 1024, 768);
 	}
-
 }
