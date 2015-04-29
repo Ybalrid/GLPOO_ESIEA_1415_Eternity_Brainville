b282bf7df0f7082ef5402b099c878fb637572060
Nicolas Gattolin <tenma@nuolezio.org>
2015-04-29 12:58:14 +0200

    Updated build and run files

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -eo pipefail
-
 if [ ! -d src ]
 then echo "Run this in the root of the project!"
 	exit 1
@@ -11,7 +9,7 @@ fi
 
 [[ $(ls bin | wc -l) > 0 ]] && rm -r bin/*
 javac -cp lib/*:bin -d bin src/main/java/*.java
-//jar -vcfm Eternity.jar Manifest.mf README.md LICENSE.txt build_and_run.* bin/ lib/ src/
+jar -vcfm Eternity.jar Manifest.mf README.md LICENSE.txt build_and_run.* bin/ lib/ src/
 java -cp lib/*:bin main.java.Eternity
 
 exit 0
