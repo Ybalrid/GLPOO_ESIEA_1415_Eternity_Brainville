1752364a06bf601dff6a09277e99019332c4ff3a
Tenma <tenma@nuolezio.org>
2015-05-02 00:10:08 +0200

    Added a grid for puzzle

--- src/main/java/PuzzlePanel.java
+++ src/main/java/PuzzlePanel.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.GridLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,6 +14,13 @@ public class PuzzlePanel extends JPanel {
 	public PuzzlePanel()
 	{
 		this.setBackground(Color.BLACK);
+		
+		this.setLayout(new GridLayout(4, 4));
+		for (int i = 0; i < 16; i++) {
+			JPanel p = new JPanel();
+			p.setBackground(new Color(i * 16, i * 16, i * 16));
+			this.add(p);
+		}
 	}
 
 	@Override
@@ -22,9 +30,6 @@ public class PuzzlePanel extends JPanel {
 		Graphics2D g2 = (Graphics2D)g;
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
-
-		g2.setColor(Color.GREEN);
-		g2.fillRect(50, 0, 200, 200);
 
 	}
 }
