ebd84217f5ff5ec65e1d4ec8588d257ddf4215cc
Ybalrid <ybalrid@cecmu.org>
2015-05-12 19:34:32 +0200

    Correctly add opencsv. Add a 'bidonCSV' just for understanding how to read CSV files

--- src/main/java/Eternity.java
+++ src/main/java/Eternity.java
@@ -1,11 +1,19 @@
 package main.java;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import main.java.bidonCSV;
 import main.java.gui.MainWindow;
 import main.java.controller.Game;
 
 public class Eternity {
 
 	public static void main(String[] args) {
+		//just messing around with CSV stuff here : 
+		bidonCSV.readAndPrint("bidon.csv");
+		
 		new Game(new MainWindow().getGamePanel());
 	}
 }
