44525cc36431132e29403bf6c5779cc9de4c2ddf
Ybalrid <ybalrid@cecmu.org>
2015-05-16 00:02:26 +0200

    write the complete content of the puzzle board to a file whenhitting the save button

--- src/main/java/model/SaveDAO.java
+++ src/main/java/model/SaveDAO.java
@@ -1,6 +1,8 @@
 package main.java.model;
 
+import java.awt.List;
 import java.io.File;
+import java.util.ArrayList;
 
 import main.java.controller.Game;
 
@@ -18,6 +20,9 @@ public class SaveDAO extends CsvDAO {
 	public void load()
 	{
 		System.out.println("Loading");
+		//Read the CSV file
+		//Apply content to the game (create faces and pieces and so on
+		//Relaunch it with theses data
 	}
 	
 	public void save()
@@ -27,16 +32,45 @@ public class SaveDAO extends CsvDAO {
 		Game game =  modelManager.getGame();
 		
 		Piece[] pieces = game.getGamePanel().getOrderedPieces();
-		
+		ArrayList<String[]> gameContent = new ArrayList<String[]>();
+
 		int debug = 0;
 		for(Piece p : pieces)
 		{
-			System.out.println("Peice : " + ++debug + " id " + p.getId() + " orientation " + p.getOrientation() + " x:" + p.getPositionX() + " y:" +  p.getPositionY());
+			//Piece ID, Piece Orient, F1 ID, F1 Pat, F1 bg, F1 FG, F2 ID, F2 pat... etc..
+			String[] pieceDesc = new String[18];
+			
+			//Integer id = new Integer(p.getId());
+			
+			pieceDesc[0] = new Integer(p.getId()).toString();
+			pieceDesc[1] = new Integer(p.getOrientation()).toString();
+			for(int i = 0; i < 4; i++)
+			{
+				pieceDesc[2 + 0 + 4*i] = new Integer(p.getFaces()[i].getId()).toString();
+				pieceDesc[2 + 1 + 4*i] = p.getFaces()[i].getPattern();
+				pieceDesc[2 + 2 + 4*i] = p.getFaces()[i].getBgColor();
+				pieceDesc[2 + 3 + 4*i] = p.getFaces()[i].getFgColor();
+			}
+			
+			for(String s : pieceDesc)
+				System.out.println(s);
+			
+			gameContent.add(pieceDesc);
+			/*System.out.println("Peice : " + ++debug + " id " + p.getId() + " orientation " + p.getOrientation() + " x:" + p.getPositionX() + " y:" +  p.getPositionY());
 			for(Face f : p.getFaces())
 			{
 				System.out.println(" Face : " + f.getId() + " patern " + f.getPattern() + " " + f.getBgColor() + " " + f.getFgColor());
-			}
+			}*/
+			
+			
 		}
+		
+		this.writeLineFromArray(gameContent);
+		
+		//Get all information in text form
+		//create an Array<Strings> that represent the content of the CSV to write
+		//wite it with 
+		//this.writeLineFromArray(array);
 	}
 
 }
